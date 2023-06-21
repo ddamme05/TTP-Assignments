b153463dcb023feec1d7a43ef355ab6ea457a187
ddamme05 <ddamme05@gmail.com>
2023-06-21 15:47:18 -0400

    initial commit

--- Assignment-04/script.js
+++ Assignment-04/script.js
@@ -1,53 +1,58 @@
-// Retrieve DOM elements
-// TODO: Get references to the required DOM elements using document.getElementById()
-
 // Array of predefined poster objects
 const posters = [
   {
-    image: 'image-url-1.jpg',
-    title: 'Poster 1',
-    quote: 'Quote 1',
+    image: 'https://www.azquotes.com/vangogh-image-quotes/17/76/Quotation-John-Locke-What-worries-you-masters-you-17-76-54.jpg',
+    title: 'Locke Motivation',
+    quote: 'What worries you, masters you.',
   },
   {
-    image: 'image-url-2.jpg',
-    title: 'Poster 2',
-    quote: 'Quote 2',
+    image: 'https://i.pinimg.com/736x/d1/ef/b3/d1efb39b11857abb2ac947f0b9543b4b--its-ok-live.jpg',
+    title: 'Living Life',
+    quote: 'It is okay to live a life others do not understand.',
   },
   {
-    image: 'image-url-3.jpg',
-    title: 'Poster 3',
-    quote: 'Quote 3',
+    image: 'https://tinyurl.com/4766p7um',
+    title: 'Mark Twain Quote',
+    quote: 'The fear of death follows from the fear of life. A man who lives fully is prepared to die at any time.',
   },
   // Add more poster objects as needed
 ];
 
 // Event listener for randomize button
 // TODO: Add an event listener to the randomizeButton that calls a function when clicked
-
+let randomizeButton = document.getElementById('randomize').addEventListener('click', generateRandomPoster);
 // Event listener for submit button
 // TODO: Add an event listener to the submitButton that calls a function when clicked
+let submitButton = document.getElementById('submit').addEventListener('click', generateCustomPoster);
 
 // Function to generate a random poster
 function generateRandomPoster() {
   // TODO: Generate a random index within the range of the posters array length
-
-  // TODO: Retrieve the random poster object from the posters array
-
+  let index = Math.floor(Math.random() * posters.length);
+  let newPoster = posters[index];
   // TODO: Call the function to update the DOM with the values from the random poster object
+  document.getElementById('poster-quote').textContent = newPoster.quote;
+  document.getElementById('poster-title').textContent = newPoster.title;
+  document.getElementById('poster-image').src = newPoster.image;
 }
 
 // Function to generate a custom poster
 function generateCustomPoster(event) {
   event.preventDefault();
 
-  // TODO: Retrieve the entered quote and author from the input fields
+  let customQuote = document.getElementById('poster-form-quote').value;
+  let customTitle = document.getElementById('poster-form-title').value;
+  let customImage = document.getElementById('poster-form-image').value;
 
-  // TODO: Create a custom poster object with the entered values
+  let customPoster = {
+    image: customImage,
+    title: customTitle,
+    quote: customQuote
+  };
 
-  // TODO: Call the function to update the DOM with the values from the custom poster object
-}
+  posters.push(customPoster);
 
-// Function to update the poster content in the DOM
-function updatePoster(imageUrl, title, quote) {
-  // TODO: Update the DOM with the values provided for the poster image, title, and quote
+  document.getElementById('poster-quote').textContent = customQuote;
+  document.getElementById('poster-title').textContent = customTitle;
+  document.getElementById('poster-image').src = customImage;
 }
