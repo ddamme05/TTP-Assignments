c2be74def2d00a98c450d72454a6af8cbb9650f0
ddamme05 <ddamme05@gmail.com>
2023-07-17 01:59:25 -0400

    Completed.

--- blogging/server.js
+++ blogging/server.js
@@ -284,7 +284,99 @@ app.delete("/posts/:id", authenticateUser, async (req, res) => {
     }
 });
 
-
+app.post("/comments", async (req, res) => {
+    try {
+      const comment = await Comment.create({
+        body: req.body.body,
+        postId: req.body.postId,
+        authorId: req.session.authorId,
+      });
+  
+      res.status(201).json({
+        message: "Comment created successfully",
+        comment,
+      });
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        res.status(422).json({ error: error.message });
+      } else if (error.name === "SequelizeUniqueConstraintError") {
+        res.status(400).json({ error: "Duplicate comment" });
+      } else {
+        console.error(error);
+        res.status(500).json({ message: "Failed to create a comment" });
+      }
+    }
+  });
+  
+  app.get("/posts/:postId/comments", async (req, res) => {
+    try {
+      const comments = await Comment.findAll({
+        where: { postId: req.params.postId },
+        include: [{ model: User, as: "author", attributes: ["name", "email"] }],
+      });
+  
+      res.status(200).json(comments);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to retrieve comments" });
+    }
+  });
+  
+  app.get("/comments/:commentId", async (req, res) => {
+    try {
+      const comment = await Comment.findByPk(req.params.commentId, {
+        include: [{ model: User, as: "author", attributes: ["name", "email"] }],
+      });
+  
+      if (!comment) {
+        return res.status(404).json({ message: "Comment not found" });
+      }
+  
+      res.status(200).json(comment);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to retrieve the comment" });
+    }
+  });
+  
+  app.put("/comments/:commentId", async (req, res) => {
+    try {
+      const comment = await Comment.findByPk(req.params.commentId);
+  
+      if (!comment) {
+        return res.status(404).json({ message: "Comment not found" });
+      }
+  
+      comment.body = req.body.body;
+      await comment.save();
+  
+      res.status(200).json({ message: "Comment updated successfully", comment });
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        res.status(422).json({ error: error.message });
+      } else {
+        console.error(error);
+        res.status(500).json({ message: "Failed to update the comment" });
+      }
+    }
+  });
+  
+  app.delete("/comments/:commentId", async (req, res) => {
+    try {
+      const comment = await Comment.findByPk(req.params.commentId);
+  
+      if (!comment) {
+        return res.status(404).json({ message: "Comment not found" });
+      }
+  
+      await comment.destroy();
+  
+      res.status(200).json({ message: "Comment deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to delete the comment" });
+    }
+  });
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
